b7ffa56f2151679506ff7c38c184ba1f82be2ed3
Fomase <82372279+Fomase@users.noreply.github.com>
2023-10-06 14:32:43 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,50 +1,136 @@
-#include <vector>
-#include <map>
-#include <string>
-#include <string_view>
+#include <iostream>
 #include <algorithm>
-#include <cassert>
+#include <list>
+#include <string>
 
 using namespace std;
 
-class Translator {
+class Editor {
 public:
-    Translator() = default;
-    
-    void Add(string_view source, string_view target) {
-        words.push_back({source.begin(), source.end()});
-        words.push_back({target.begin(), target.end()});
-        auto& sproxy = *find(words.begin(), words.end(), source);
-        auto& tproxy = *find(words.begin(), words.end(), target);
-        sources[sproxy] = tproxy;
-        targets[tproxy] = sproxy;
+    Editor() {
+        iter = text.begin();
     }
     
-    string_view TranslateForward(string_view source) const {
-        return sources.at(source);
+    // сдвинуть курсор влево
+    void Left() {
+        if (iter != text.begin()) {
+            auto tmpiter = iter;
+            --iter;
+            swap(*iter, *tmpiter);
+        }
     }
     
-    string_view TranslateBackward(string_view target) const {
-        return targets.at(target);
+    // сдвинуть курсор вправо 
+    void Right() {
+        if (iter != text.end()) {
+            auto tmpiter = iter;
+            ++iter;
+            swap(*tmpiter, *iter);
+        }
     }
+ 
+    // вставить символ token
+    void Insert(char token) {
+        text.insert(iter, token);
+    }
+    
+    // вырезать не более tokens символов, начиная с текущей позиции курсора
+    void Cut(size_t tokens = 1) {
+        auto tmpbegin = iter;
+        auto tmpend = iter;
+        if (tokens >= 1) {
+            ++tmpbegin;
+        }
+        auto dist = min(tokens, static_cast<size_t>(distance(iter, text.end())));
+        advance(tmpend, dist);
 
-private:
-    std::map<string_view, string_view> sources;
-    std::map<string_view, string_view> targets;
-    std::vector<std::string> words;
-};
+        if (tmpend != text.end()) {
+            ++tmpend;
+        }
 
-void TestSimple() {
-    Translator translator;
-    translator.Add(string("okno"s), string("window"s));
-    translator.Add(string("stol"s), string("table"s));
-
-    assert(translator.TranslateForward("okno"s) == "window"s);
-    assert(translator.TranslateBackward("table"s) == "stol"s);
-    assert(translator.TranslateForward("table"s) == ""s);
-}
+        buffer.erase(buffer.begin(), buffer.end());
+        buffer.insert(buffer.begin(), tmpbegin, tmpend);
+        --tmpend;
+        text.erase(tmpbegin, tmpend);
+        text.erase(tmpend);
+    }
+    
+    // cкопировать не более tokens символов, начиная с текущей позиции курсора
+    void Copy(size_t tokens = 1) {
+        if (tokens >= 1) {
+            ++iter;
+        }
+        auto dist = min(tokens, static_cast<size_t>(distance(iter, text.end())));
+        auto tmpiter = iter;
+        advance(tmpiter, dist);
+        buffer.erase(buffer.begin(), buffer.end());
+        buffer.insert(buffer.begin(), iter, tmpiter);
+    }
+    
+    // вставить содержимое буфера в текущую позицию курсора
+    void Paste() {
+        text.insert(iter, buffer.begin(), buffer.end());
+    }
+    
+    // получить текущее содержимое текстового редактора
+    string GetText() const {
+        string output = "";
+        for (auto ch : text) {
+            output += ch;
+        }
+        return output;
+    }
+ 
+ private:
+    list<char> text = {'|'};
+    list<char> buffer;
+    list<char>::iterator iter = text.begin();
+}; 
 
 int main() {
-    TestSimple();
+    Editor editor;
+    const string text = "hello, world";
+    for (char c : text) {
+        editor.Insert(c);
+    }
+    cout << editor.GetText() << endl; 
+    // Текущее состояние редактора: `hello, world|`
+    for (size_t i = 0; i < text.size(); ++i) {
+        editor.Left();
+    }
+    cout << editor.GetText() << endl; 
+    // Текущее состояние редактора: `|hello, world`
+    editor.Cut(7);
+    
+    cout << editor.GetText() << endl; 
+    // Текущее состояние редактора: `|world`
+    // в буфере обмена находится текст `hello, `
+    for (size_t i = 0; i < 5; ++i) {
+        editor.Right();
+    
+
+    }
+    cout << editor.GetText() << endl; 
+    // Текущее состояние редактора: `world|`
+    editor.Insert(',');
+    
+
+    
+    editor.Insert(' ');
+    
+    cout << editor.GetText() << endl; 
+    // Текущее состояние редактора: `world, |`
+    editor.Paste();
+    
+    cout << editor.GetText() << endl; 
+    // Текущее состояние редактора: `world, hello, |`
+    editor.Left();
+    editor.Left();
+    cout << editor.GetText() << endl; 
+    //Текущее состояние редактора: `world, hello|, `
+    editor.Cut(3);  // Будут вырезаны 2 символа
+    cout << editor.GetText() << endl; 
+    // Текущее состояние редактора: `world, hello|`
+    cout << editor.GetText();
     return 0;
 }
